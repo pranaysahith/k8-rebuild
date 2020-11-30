17241c4929e1e3265204a50ec810817732b9c42a
NourEddine Yassine <noureddinex@protonmail.com>
2020-11-30 19:56:58 +0200

    Update dependencies
    Fix error when running in environment with bitnami repo not installed
    > Error: found in Chart.yaml, but missing in charts/ directory: common

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -2,4 +2,5 @@
 git clone https://github.com/k8-proxy/sow-rest-api.git
 helm upgrade --install sow-rest-api sow-rest-api/chart
 git clone --branch develop https://github.com/k8-proxy/sow-rest-UI.git
+helm dependency update sow-rest-UI/chart/
 helm upgrade --install sow-rest-ui sow-rest-UI/chart
