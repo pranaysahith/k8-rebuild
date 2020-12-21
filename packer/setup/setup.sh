01b83818134fc09fe783dfdd27c271d5e9b496a7
Pranay Bejgum <prbejgum@microsoft.com>
2020-12-21 19:36:36 +0530

    install helm using different method

--- packer/setup/setup.sh
+++ packer/setup/setup.sh
@@ -10,10 +10,11 @@ chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 echo "Done installing kubectl"
 
-curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
-echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
-sudo apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get install helm -y
+# curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+# echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+# sudo apt-get update
+# sudo DEBIAN_FRONTEND=noninteractive apt-get install helm -y
+curl -sfL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 echo "Done installing helm"
 
 # install k3s
@@ -21,4 +22,5 @@ curl -sfL https://get.k3s.io | sh -
 
 # install helm chart
 git clone https://github.com/k8-proxy/k8-rebuild.git --recursive && cd k8-rebuild && git submodule foreach git pull origin main
+sleep 120s
 helm upgrade --install sow-rest kubernetes/
