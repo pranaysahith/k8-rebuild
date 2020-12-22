e0c45a7b6af29f1bb0a614fd73cfa8b525758b8a
pranaysahith <pranaysahith7@gmail.com>
2020-12-22 20:45:22 +0530

    remove comments

--- packer/setup/setup.sh
+++ packer/setup/setup.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# This is a placeholder script, you can move your setup script here to install some custom deployment on the VM
-# The parent directory of this script will be transferred with its content to the VM under /tmp/setup path
-# (i.e: useful for copying configs, scripts, systemd units, etc..)  
-
 # install k3s
 curl -sfL https://get.k3s.io | sh -
 mkdir ~/.kube && sudo install -T /etc/rancher/k3s/k3s.yaml ~/.kube/config -m 600 -o $USER
