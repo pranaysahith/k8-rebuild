d7536628e71f83acfba97022dbbf0f9bcd5a9399
pranaysahith <pranaysahith7@gmail.com>
2021-01-06 18:25:19 +0530

    update ubuntu version

--- packer/setup/setup.sh
+++ packer/setup/setup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# install docker
+sudo apt-get update && sudo apt-get install curl git -y
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo usermod -aG docker $( whoami )
+
 # install k3s
 curl -sfL https://get.k3s.io | sh -
 mkdir ~/.kube && sudo install -T /etc/rancher/k3s/k3s.yaml ~/.kube/config -m 600 -o $USER
