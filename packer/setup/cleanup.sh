05bc0cab4bd166ba70b2925864348aaa1be3380a
pranaysahith <pranaysahith7@gmail.com>
2021-01-07 20:02:36 +0530

    update base image to ubuntu 18

--- packer/setup/cleanup.sh
+++ packer/setup/cleanup.sh
@@ -9,12 +9,12 @@ sudo logrotate --force /etc/logrotate.conf
 sudo journalctl --rotate && sudo journalctl --vacuum-size=1
 # Network clean up
 sudo rm -f /etc/netplan/*.yml /etc/netplan/*.yaml
-sudo tee /etc/netplan/network.yaml >/dev/null <<EOF
-network:
-  version: 2
-  ethernets:
-    eth0:
-      dhcp4: true
-EOF
+# sudo tee /etc/netplan/network.yaml >/dev/null <<EOF
+# network:
+#   version: 2
+#   ethernets:
+#     eth0:
+#       dhcp4: true
+# EOF
 # Shell history clean up
 history -c && history -w
